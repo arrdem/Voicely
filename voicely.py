2b92d207918fee7454c5c2c6d5002c4451d629da
rmckenzie <rmckenzie92@gmail.com>
2012-06-05 11:46:37 -0500

    remember the name... and add the new config file

--- voicely.py
+++ voicely.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
-# Copyright 2008 Ben Duffield, 2012 Reid McKenzie
+#
+# Copyright 2008 Ben Duffield as Voximp,
+#           2012 Reid McKenzie as Voicely
+#
 # --MIT License-----------------------------------------------------------------
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -19,6 +22,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 
 #  StdLib imports...
 from ConfigParser import ConfigParser
@@ -28,17 +32,45 @@ import sys
 import md5
 import os
 
-# Depend. imports...
-import gobject
-import pygtk
-import pygst
-import gtk
-pygtk.require('2.0')
-pygst.require('0.10')
-gobject.threads_init()
-import gst
 
-class Voximp(object):
+__version__ = '0.1.0'
+
+__v_usage__ = """
+Usage: Voicely.py [options]
+Options:
+    -v, --version     show program version and exit
+    -h, --help        show this help message and exit
+    -c, --config dir  use a nonstandard config file location
+
+About:
+    Voicely version {0}, a highly configurable voice command interface for
+    Linux. Copyright 2008 Ben Duffield (bavardage@archlinux.us) as Voximp,
+    updated and maintained by Reid McKenzie (rmckenzie92@gmail.com)
+""".format(__version__)
+
+__no_conf_file_err__ = """
+[FATAL] No configuration file found.
+....    Check that {0}Voicely.conf exists and contains command
+....    specifications as documented in the README.
+[EXITING]
+"""
+
+__no_conf_dir_err__ = """
+[FATAL] No configuration dir found.
+....    Check that ~/.config/Voicely/Voicely.conf exists and contains command
+....    specifications as documented in the README.
+[EXITING]
+"""
+
+__hash_not_found__ = """
+[FATAL] No checksum file (or a non-matching checksum file) was found for your
+        configuration file. As a result you need to follow the instructions
+        below and generate new Sphinx corpus files before Voicely will support
+        your new instructions.
+"""
+
+
+class Voicely(object):
     dial = None
 
     def __init__(self, config, dbg=False):
@@ -49,9 +81,11 @@ class Voximp(object):
         self.pipeline.set_state(gst.STATE_PLAYING)
 
     def init_gst(self):
-        self.pipeline = gst.parse_launch('alsasrc ! audioconvert ! audioresample '
-                                         + '! vader name=vad auto-threshold=true '
-                                         + '! pocketsphinx name=asr ! fakesink')
+        self.pipeline = gst.parse_launch(
+            'alsasrc ! audioconvert ! audioresample ! ' +
+            ' vader name=vad auto-threshold=true ' +
+            '! pocketsphinx name=asr ! fakesink')
+
         asr = self.pipeline.get_by_name('asr')
         asr.connect('partial_result', self.asr_partial_result)
         asr.connect('result', self.asr_result)
@@ -85,10 +119,12 @@ class Voximp(object):
             self.final_result(msg.structure['hyp'], msg.structure['uttid'])
 
     def partial_result(self, hyp, uttid):
-        if(self.dbg): print "partial: %s" % hyp
+        if(self.dbg):
+            print ("partial: %s" % hyp)
 
     def final_result(self, hyp, uttid):
-        if(self.dbg): print "final: %s" % hyp
+        if(self.dbg):
+            print ("final: %s" % hyp)
         prog = ''
         command = None
         if self.dial is not None:
@@ -116,108 +152,135 @@ class Voximp(object):
             Popen(prog, shell=True)
 
     def confirm(self, prog):
-        if(self.dbg): print "Confirming %s" % prog
-        self.dial = gtk.MessageDialog(message_format = "Confirm?", type=gtk.MESSAGE_QUESTION)
-        self.dial.format_secondary_markup("Say <b><i>yes</i></b> or <b><i>no</i></b>")
+        if(self.dbg):
+            print "Confirming %s" % prog
+
+        self.dial = gtk.MessageDialog(message_format="Confirm?",
+                                      type=gtk.MESSAGE_QUESTION)
+        self.dial.format_secondary_markup(
+                                    "Say <b><i>yes</i></b> or <b><i>no</i></b>")
         self.dial.prog = prog
         self.dial.show_all()
         self.dial.connect("response", self.confirmCallback)
 
     def confirmCallback(self, dialog, response_id):
-        if(self.dbg): print "callback called back"
+        if(self.dbg):
+            print "callback called back"
+
         if response_id == gtk.RESPONSE_YES:
             p = Popen(dialog.prog, shell=True)
             self.dial.destroy()
         self.dial = None
 
-__version__ = '0.1.0'
-
-__usage__ = """
-Usage: voximp.py [options]
-Options:
-    -v, --version     show program version and exit
-    -h, --help        show this help message and exit
-About:
-    Voximp version {0}, a highly configurable voice command interface for
-    Linux. Copyright 2008 Ben Duffield (bavardage@archlinux.us), updated and
-    maintained by Reid McKenzie (rmckenzie92@gmail.com)
-""".format(__version__)
 
 def corpus(config):
-    words = config.keys()
-    corpusText =  "\n".join(words)
+    words = set(config.keys())
+    words.remove('__settings__')
+    corpusText = "\n".join(words)
     filename = os.path.join(os.getcwd(), 'corpus.txt')
     corp = open(filename, 'w')
-    corp.write(corpusText)
+    corp.write(corpusText + '\n')
     corp.flush()
     corp.close()
 
-    print "Corpus saved to %s" % filename
-    print "Now visit http://www.speech.cs.cmu.edu/tools/lmtool.html"
-    print "  ==> choose the corpus file, click COMPILE KNOWLEDGE BASE"
-    print "  ==> save the three files to ~/.config/voximp/"
-    print "  ==> edit ~/.config/voximp/voximpconf.py and set the languagemodel string to the appropriate value \n\t- e.g. if the files are named 4766.dic, 4766.lm and 4766.sent, set languagemodel = '4766'"
+    print ("Corpus saved to %s" % filename)
+    print """Now visit http://www.speech.cs.cmu.edu/tools/lmtool.html
+  ==> choose the corpus file, click COMPILE KNOWLEDGE BASE"
+  ==> save the three files to ~/.config/Voicely/"
+  ==> edit ~/.config/Voicely/Voicelyconf.py and set the lang field of the
+      __settings__ section to the appropriate value - e.g. if the files are
+      named 4766.dic, 4766.lm and 4766.sent, set lang = 4766"""
 
 
 if __name__ == '__main__':
-    config_dir  = os.path.expanduser("~/") + '.config/voximp/'
-    config_file = 'voximp.conf'
-    config = None
+    config_dir = os.path.expanduser("~/") + '.config/voicely/'
+    config_file = 'voicely.conf'
+    config_data = None
 
-    if not os.path.isdir(config_dir):
+    c_flag = False
+    for opt in sys.argv[1:]:
+        if c_flag:
+            c_flag = False
+
+            if(os.path.exists(opt) and os.path.isdir(opt)):
+                config_dir = opt
+                continue
+            else:
+                print ("[FATAL] Specified config dir does not exist")
+                exit(2)
+
+        if opt in ("-h", "--help"):
+            print __v_usage__
+            sys.exit()
+
+        elif opt in ("-v", "--version"):
+            print __version__
+            sys.exit()
+
+        elif opt in ('-c', '--config'):
+            c_flag = True
+
+        else:
+            print ("[WARNING] Unknown option %s" % opt)
+
+    # Depend. imports...
+    import gobject
+    import pygtk
+    import pygst
+    import gtk
+    pygtk.require('2.0')
+    pygst.require('0.10')
+    gobject.threads_init()
+    import gst
+
+    if not (os.path.isdir(config_dir)):
         # the configurations are non-existant as far as this code can tell
         os.makedirs(config_dir)
-        open(config_dir + os.sep + config_file, 'w').write('\n') # touch conf file
-        print "[FATAL] No configuration file found.\n" +\
-              ".......     Check that ~/.config/voximp/voximp.conf exists and contains command\n" +\
-              ".......     specifications as documented.\n" +\
-              "[EXITING]"
+        open(config_dir + config_file, 'w').write('\n')
+        print __no_conf_dir_err__
+        exit(1)
+
+    elif not os.path.exists(config_dir + config_file):
+        open(config_dir + config_file, 'w').write('\n')
+        print __no_conf_file_err__.format(config_dir)
         exit(1)
 
     else:
         # the configs are assumed to be present, check hash of config file to
         # see if the language files need to be recalculated.
         #
-        # the hash file is '~/.config/voximp/hash'
-        # the config file is '~/.config/voximp/voximp.conf'
+        # the hash file is '~/.config/Voicely/hash'
+        # the config file is '~/.config/Voicely/Voicely.conf'
 
-        hf = open(config_dir + 'hash', 'r+')
-        cf = config_dir + 'voximp.conf'
+        hf = config_dir + 'hash'
+        cf = config_dir + 'Voicely.conf'
 
         hash = md5.new()
+        hf_data = open(hf).read() if os.path.exists(hf) else ""
         hash.update(open(cf).read())
 
         a = ConfigParser()
         a.read(cf)
 
-        config = {b: dict(a.items(b)) for b in a.sections()}
+        config_data = {b: dict(a.items(b)) for b in a.sections()}
 
-        if(hash.hexdigest() != hf.read()):
+        if(hash.hexdigest() != hf_data):
+            print __hash_not_found__
             # this is the recalculate language file case
-            corpus(config)
+            corpus(config_data)
 
             # and update the hash...
-            hf.seek(0, 0)
+            hf = open(hf, 'w')
             hf.write(hash.hexdigest())
             hf.close()
+            exit(1)
 
-        language_file = os.path.join(config_dir, config['__settings__']['lang'])
+        language_file = os.path.join(config_dir,
+                                     config_data['__settings__']['lang'])
 
-        config['hmm'] = '/usr/share/pocketsphinx/model/hmm/wsj1'
-        config['lm'] = '{0}.lm'.format(language_file)
-        config['dict'] = '{0}.dic'.format(language_file)
+        config_data['hmm'] = '/usr/share/pocketsphinx/model/hmm/wsj1'
+        config_data['lm'] = '{0}.lm'.format(language_file)
+        config_data['dict'] = '{0}.dic'.format(language_file)
 
-    opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "version"])
-
-    for opt, _ in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-
-        elif opt in ("-v", "--version"):
-            version()
-            sys.exit()
-
-    app = Voximp(config)
+    app = Voicely(config_data)
     gtk.main()
-
