ce326132f3b2fd3fa54846c2e463437a83d1a88f
rmckenzie <rmckenzie92@gmail.com>
2012-06-05 09:10:22 -0500

    Rewrote the config file code to use a standard .conf instead of the retarded .py import *

--- voximp.py
+++ voximp.py
@@ -1,52 +1,50 @@
 #!/usr/bin/env python2
+# Copyright 2008 Ben Duffield, 2012 Reid McKenzie
+# --MIT License-----------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# Copyright (c) 2008 Ben Duffield
-# Licensing - no idea
-# Probably w/e sphinx is, think it's MIT
-# Be nice!
+#  StdLib imports...
+from ConfigParser import ConfigParser
+from subprocess import Popen
+import getopt
+import sys
+import md5
+import os
 
-#######
-#REQUIRES:
-# gstreamer
-# pygtk
-# pocketsphinx
-# xdotool
-#########
-
-import pygtk
-pygtk.require('2.0')
-import gtk
-
+# Depend. imports...
 import gobject
+import pygtk
 import pygst
+import gtk
+pygtk.require('2.0')
 pygst.require('0.10')
 gobject.threads_init()
 import gst
-from subprocess import Popen
-import os
-import sys
-import getopt
-
-
-config_dir = 'config/'
-try:
-    os.makedirs(config_dir)
-except:
-    pass
-sys.path.append(config_dir)
-from voximpconf import *
-
-language_file = os.path.join(config_dir, str(languagemodel))
-
-config = {
-    'hmm': '/usr/share/pocketsphinx/model/hmm/wsj1',
-    'lm': '%s.lm' % language_file,
-    'dict': '%s.dic' % language_file
-    }
 
 class Voximp(object):
     dial = None
-    def __init__(self):
+
+    def __init__(self, config, dbg=False):
+        self.config = config
+        self.dbg = dbg
+
         self.init_gst()
         self.pipeline.set_state(gst.STATE_PLAYING)
 
@@ -57,8 +55,8 @@ class Voximp(object):
         asr = self.pipeline.get_by_name('asr')
         asr.connect('partial_result', self.asr_partial_result)
         asr.connect('result', self.asr_result)
-        asr.set_property('lm', config['lm'])
-        asr.set_property('dict', config['dict'])
+        asr.set_property('lm', self.config['lm'])
+        asr.set_property('dict', self.config['dict'])
         asr.set_property('configured', True)
 
         bus = self.pipeline.get_bus()
@@ -85,14 +83,12 @@ class Voximp(object):
             self.partial_result(msg.structure['hyp'], msg.structure['uttid'])
         elif msgtype == 'result':
             self.final_result(msg.structure['hyp'], msg.structure['uttid'])
-            #self.pipeline.set_state(gst.STATE_PAUSED)
-            #self.button.set_active(False)
 
     def partial_result(self, hyp, uttid):
-        print "partial: %s" % hyp
+        if(self.dbg): print "partial: %s" % hyp
 
     def final_result(self, hyp, uttid):
-        print "final: %s" % hyp
+        if(self.dbg): print "final: %s" % hyp
         prog = ''
         command = None
         if self.dial is not None:
@@ -100,34 +96,27 @@ class Voximp(object):
                 self.dial.response(gtk.RESPONSE_YES)
             else:
                 self.dial.response(gtk.RESPONSE_NO)
-        elif hyp in programcommand:
-            prog = programcommand[hyp]
+        elif hyp in self.config:
+            prog = self.config[hyp]['cmd']
             command = hyp
-        elif hyp in keycommand:
-            prog = "xdotool key ``%s''" % keycommand[hyp]
-            command = hyp
-        elif hyp in mousecommand:
-            prog = "xdotool click %s" % mousecommand[hyp]
-            command = hyp
-        else:
-            values = hyp.split(' ')
-            if len(values) <= 1:
-                return
-            if values[0] in progswithargs:
-                prog = progswithargs[values[0]] + ' ' + ' '.join(values)
-                command = values[0]
-            else:
-                for value in values:
-                    self.final_result(value, 0)
+        #else:
+            #values = hyp.split(' ')
+            #if len(values) <= 1:
+                #return
+            #if values[0] in progswithargs:
+                #prog = progswithargs[values[0]] + ' ' + ' '.join(values)
+                #command = values[0]
+            #else:
+                #for value in values:
+                    #self.final_result(value, 0)
         if prog:
-            print "command is %s" % command
-            if command in confirm:
-                self.confirm(prog)
-            else:
-                p = Popen(prog, shell=True)
+            if(self.dbg):
+                print "command is %s" % command
+
+            Popen(prog, shell=True)
 
     def confirm(self, prog):
-        print "Confirming %s" % prog
+        if(self.dbg): print "Confirming %s" % prog
         self.dial = gtk.MessageDialog(message_format = "Confirm?", type=gtk.MESSAGE_QUESTION)
         self.dial.format_secondary_markup("Say <b><i>yes</i></b> or <b><i>no</i></b>")
         self.dial.prog = prog
@@ -135,63 +124,100 @@ class Voximp(object):
         self.dial.connect("response", self.confirmCallback)
 
     def confirmCallback(self, dialog, response_id):
-        print "callback called back"
+        if(self.dbg): print "callback called back"
         if response_id == gtk.RESPONSE_YES:
             p = Popen(dialog.prog, shell=True)
             self.dial.destroy()
         self.dial = None
 
-versionNumber = '0.0.1'
-usageInfo = '''Usage: voximp [options]
+__version__ = '0.1.0'
 
+__usage__ = """
+Usage: voximp.py [options]
 Options:
-  -v, --version     show program version and exit
-  -h, --help        show this help message and exit
-  -c, --corpus      create a corpus.txt in current directory - used for generating language model files
-'''
+    -v, --version     show program version and exit
+    -h, --help        show this help message and exit
+About:
+    Voximp version {0}, a highly configurable voice command interface for
+    Linux. Copyright 2008 Ben Duffield (bavardage@archlinux.us), updated and
+    maintained by Reid McKenzie (rmckenzie92@gmail.com)
+""".format(__version__)
 
-def usage():
-    print "Voximp version %s" % versionNumber
-    print usageInfo
-
-def version():
-    print "Version %s" % versionNumber
-
-def corpus():
-    words = []
-    words.extend(keycommand.keys())
-    words.extend(programcommand.keys())
-    words.extend(mousecommand.keys())
-    words.extend(progswithargs.keys())
+def corpus(config):
+    words = config.keys()
     corpusText =  "\n".join(words)
     filename = os.path.join(os.getcwd(), 'corpus.txt')
-    print "Saving to %s" % filename
     corp = open(filename, 'w')
     corp.write(corpusText)
     corp.flush()
     corp.close()
-    print "Corpus saved"
+
+    print "Corpus saved to %s" % filename
     print "Now visit http://www.speech.cs.cmu.edu/tools/lmtool.html"
     print "  ==> choose the corpus file, click COMPILE KNOWLEDGE BASE"
     print "  ==> save the three files to ~/.config/voximp/"
     print "  ==> edit ~/.config/voximp/voximpconf.py and set the languagemodel string to the appropriate value \n\t- e.g. if the files are named 4766.dic, 4766.lm and 4766.sent, set languagemodel = '4766'"
 
+
 if __name__ == '__main__':
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hcv", ["help", "corpus", "version"])
-    except getopt.GetoptError:
-        print "error"
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
+    config_dir  = os.path.expanduser("~/") + '.config/voximp/'
+    config_file = 'voximp.conf'
+    config = None
+
+    if not os.path.isdir(config_dir):
+        # the configurations are non-existant as far as this code can tell
+        os.makedirs(config_dir)
+        open(config_dir + os.sep + config_file, 'w').write('\n') # touch conf file
+        print "[FATAL] No configuration file found.\n" +\
+              ".......     Check that ~/.config/voximp/voximp.conf exists and contains command\n" +\
+              ".......     specifications as documented.\n" +\
+              "[EXITING]"
+        exit(1)
+
+    else:
+        # the configs are assumed to be present, check hash of config file to
+        # see if the language files need to be recalculated.
+        #
+        # the hash file is '~/.config/voximp/hash'
+        # the config file is '~/.config/voximp/voximp.conf'
+
+        hf = open(config_dir + 'hash', 'r+')
+        cf = config_dir + 'voximp.conf'
+
+        hash = md5.new()
+        hash.update(open(cf).read())
+
+        a = ConfigParser()
+        a.read(cf)
+
+        config = {b: dict(a.items(b)) for b in a.sections()}
+
+        if(hash.hexdigest() != hf.read()):
+            # this is the recalculate language file case
+            corpus(config)
+
+            # and update the hash...
+            hf.seek(0, 0)
+            hf.write(hash.hexdigest())
+            hf.close()
+
+        language_file = os.path.join(config_dir, config['__settings__']['lang'])
+
+        config['hmm'] = '/usr/share/pocketsphinx/model/hmm/wsj1'
+        config['lm'] = '{0}.lm'.format(language_file)
+        config['dict'] = '{0}.dic'.format(language_file)
+
+    opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "version"])
+
+    for opt, _ in opts:
         if opt in ("-h", "--help"):
             usage()
             sys.exit()
-        elif opt in ("-c", "--corpus"):
-            corpus()
-            sys.exit()
+
         elif opt in ("-v", "--version"):
             version()
             sys.exit()
-    app = Voximp()
+
+    app = Voximp(config)
     gtk.main()
+
